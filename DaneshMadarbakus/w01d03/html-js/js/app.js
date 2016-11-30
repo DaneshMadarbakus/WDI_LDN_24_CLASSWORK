0de813ae88bb07caa6e709473374edebe9c59225
DaneshMadarbakus <daneshplt@hotmail.co.uk>
2016-11-30 17:39:24 +0000

    w01d03 classwork

---
+++ DaneshMadarbakus/w01d03/html-js/js/app.js
@@ -0,0 +1,9 @@
+alert('Finish this tune');
+var song1 = prompt('Youve been hit by, youve been struck by a...');
+while (song1 !== 'smooth criminal') {
+  // alert('try again');
+  song1 = prompt('try again');
+  if (song1 === 'smooth criminal'){
+    alert('nice');
+  }
+}
