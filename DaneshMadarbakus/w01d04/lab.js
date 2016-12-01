d2e54769b26a1191e956ac9339433e909ab3ecca
DaneshMadarbakus <daneshplt@hotmail.co.uk>
2016-12-01 18:24:17 +0000

    w01d04 classwork

---
+++ DaneshMadarbakus/w01d04/lab.js
@@ -0,0 +1,77 @@
+// Question 1
+  function maxOfTwoNumbers(a, b){
+    if(a > b){
+      return a;
+    } else if(a === b){
+      return 'same value';
+    } else {
+      return b;
+    }
+  }
+
+// Question 2
+function maxOfThree(a, b, c) {
+  if ((a >= b) && (a >= c)){
+    return a;
+  } else if ((b >= c) && (b >= a)){
+    return b;
+  } else {
+    return c;
+  }
+}
+
+// Question 3
+function isCharacterAVowel(x) {
+  x = x.toLowerCase();
+  if (x === 'a' || x === 'e' || x === 'i' || x === 'o' || x === 'u') {
+    return 'It is a vowel';
+  } else {
+    return 'It is not a vowel';
+  }
+}
+
+// Question 4
+function sumArray(a) {
+
+}
+
+
+// Question 4
+function multiplyArray() {
+
+}
+
+
+// Question 5
+var numberOfArguments = function(){
+
+}
+
+
+
+// Question 6
+var reverseString = function (){
+
+};
+
+
+// Question 7
+function findLongestWord () {
+
+}
+
+
+// Question 8
+function filterLongWords () {
+
+}
+
+
+// Bonus 1
+//??????
+
+
+// Bonus 2
+function charactersOccurencesCount() {
+
+}
