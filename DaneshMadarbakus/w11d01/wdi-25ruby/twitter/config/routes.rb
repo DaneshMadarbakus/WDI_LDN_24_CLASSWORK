27ba1a75c24083d16efb80d0069681c2029a9606
DaneshMadarbakus <daneshplt@hotmail.co.uk>
2017-04-10 17:30:28 +0100

    building authentication in ruby on rails

---
+++ DaneshMadarbakus/w11d01/wdi-25ruby/twitter/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  scope :api do
+    resources :users, except: [:create]
+    resources :posts
+    post 'register', to: 'authentications#register'
+    post 'login', to: 'authentications#login'
+    post 'oauth/github', to: 'oauth#github'
+  end
+end
